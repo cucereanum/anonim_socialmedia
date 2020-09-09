ae7f0fef1bdac3e221acf524d102ba1ee3c42cc4
cucereanum <cucereanum@email.com>
2020-09-09 10:25:41 +0300

    Update for cloud

--- index.js
+++ index.js
@@ -18,14 +18,17 @@ if (!config.get("jwtPrivateKey")) {
   process.exit(1);
 }
 
+const connection_url =
+  "mongodb+srv://admin:Mmn2RDk1HQBcor9O@cluster0.vew6i.mongodb.net/Pixel-DB?retryWrites=true&w=majority";
+
 mongoose
-  .connect("mongodb://localhost/my-app", {
+  .connect(connection_url, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("Connected to MongoDb..."))
-  .catch((err) => console.log("Could not connect to MongoDb...", err));
+  .then(() => console.log("Connected to MongoDb Cloud..."))
+  .catch((err) => console.log("Could not connect to MongoDb Cloud...", err));
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
